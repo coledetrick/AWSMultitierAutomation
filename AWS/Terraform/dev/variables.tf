082914679fe8f54eb115972fcf356cb7ab1dc969
Cole Detrick <153991478+coledetrick@users.noreply.github.com>
2025-12-31 12:54:13 -0600

    Update variables.tf

--- AWS/Terraform/dev/variables.tf
+++ AWS/Terraform/dev/variables.tf
@@ -6,3 +6,43 @@ resource "aws_vpc" "main" {
     Name = "automated-VPC"
   }
 }
+
+resource "aws_subnet" "PublicSubnet01AZ01" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/28"
+  availability_zone = "us-west-2c"
+
+  tags = {
+    Name = "PublicSubnet01AZ01"
+  }
+}
+
+resource "aws_subnet" "PrivateSubnet01AZ01" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.16/28"
+  availability_zone = "us-west-2c"
+
+  tags = {
+    Name = "PrivateSubnet01AZ01"
+  }
+}
+
+resource "aws_subnet" "PublicSubnet01AZ02" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.32/28"
+  availability_zone = "us-west-2d"
+
+  tags = {
+    Name = "PublicSubnet01AZ02"
+  }
+}
+
+resource "aws_subnet" "PrivateSubnet01AZ02" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.48/28"
+  availability_zone = "us-west-2d"
+
+  tags = {
+    Name = "PrivateSubnet01AZ02"
+  }
+}
