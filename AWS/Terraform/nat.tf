517d4e99efb4293d499d65eb91eaa4ebf2829550
Cole Detrick <153991478+coledetrick@users.noreply.github.com>
2025-12-31 11:06:01 -0600

    Add NAT Gateway and EIP resources in Terraform

---
+++ AWS/Terraform/nat.tf
@@ -0,0 +1,13 @@
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags   = { Name = "VPC01-NAT-EIP" }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.subnets["public_az01"].id
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = { Name = "VPC01-NATGW" }
+}
