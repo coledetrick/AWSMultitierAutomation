5acfd98ebc2872a4fb44ec72e8aa9d3c8623ff8f
Cole Detrick <153991478+coledetrick@users.noreply.github.com>
2025-12-31 13:21:52 -0600

    Add public and private route tables in Terraform

--- AWS/Terraform/dev/variables.tf
+++ AWS/Terraform/dev/variables.tf
@@ -53,3 +53,29 @@ resource "aws_subnet" "PrivateSubnet01AZ02" {
     Name = "PrivateSubnet01AZ02"
   }
 }
+
+resource "aws_route_table" "PublicRouteTable01" {
+  vpc_id = aws_vpc.example.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable01"
+  }
+}
+
+resource "aws_route_table" "PrivateRouteTable01" {
+  vpc_id = aws_vpc.example.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "PrivateRouteTable01"
+  }
+}
