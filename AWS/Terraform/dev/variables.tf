3d2514531bcfa5403cbd19c034609bddc77ea95b
Cole Detrick <153991478+coledetrick@users.noreply.github.com>
2026-01-04 21:37:34 -0600

    Update variables.tf

--- AWS/Terraform/dev/variables.tf
+++ AWS/Terraform/dev/variables.tf
@@ -17,51 +17,51 @@ resource "aws_nat_gateway" "main" {
 }
 
 
-resource "aws_subnet" "PublicSubnet01AZ01" {
+resource "aws_subnet" "public_subnet01az01" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/28"
   availability_zone = "us-west-2c"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "PublicSubnet01AZ01"
+    Name = "public_subnet01az01"
   }
 }
 
-resource "aws_subnet" "PrivateSubnet01AZ01" {
+resource "aws_subnet" "private_subnet01az01" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.16/28"
   availability_zone = "us-west-2c"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "PrivateSubnet01AZ01"
+    Name = "private_subnet01az01"
   }
 }
 
-resource "aws_subnet" "PublicSubnet01AZ02" {
+resource "aws_subnet" "public_subnet01az02" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.32/28"
   availability_zone = "us-west-2d"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "PublicSubnet01AZ02"
+    Name = "public_subnet01az02"
   }
 }
 
-resource "aws_subnet" "PrivateSubnet01AZ02" {
+resource "aws_subnet" "private_subnet01az02" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.48/28"
   availability_zone = "us-west-2d"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "PrivateSubnet01AZ02"
+    Name = "private_subnet01az02"
   }
 }
 
-resource "aws_route_table" "PublicRouteTable01" {
+resource "aws_route_table" "public_route_table01" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -70,11 +70,11 @@ resource "aws_route_table" "PublicRouteTable01" {
   }
 
   tags = {
-    Name = "PublicRouteTable01"
+    Name = "public_route_table01"
   }
 }
 
-resource "aws_route_table" "PrivateRouteTable01" {
+resource "aws_route_table" "private_route_table01" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -83,34 +83,33 @@ resource "aws_route_table" "PrivateRouteTable01" {
   }
 
   tags = {
-    Name = "PrivateRouteTable01"
+    Name = "private_route_table01"
   }
 }
 
-resource "aws_route_table_association" "PubSub01AZ01Assoc" {
-  subnet_id      = aws_subnet.PublicSubnet01AZ01.id
-  route_table_id = aws_route_table.PublicRouteTable01.id
+resource "aws_route_table_association" "pub_sub01az01assoc" {
+  subnet_id      = aws_subnet.public_subnet01az01.id
+  route_table_id = aws_route_table.public_route_table01.id
 }
 
-resource "aws_route_table_association" "PubSub01AZ02Assoc" {
-  subnet_id      = aws_subnet.PublicSubnet01AZ02.id
-  route_table_id = aws_route_table.PublicRouteTable01.id
+resource "aws_route_table_association" "pub_sub01az02assoc" {
+  subnet_id      = aws_subnet.public_subnet01az02.id
+  route_table_id = aws_route_table.public_route_table01.id
 }
 
-resource "aws_route_table_association" "PrivSub01AZ01Assoc" {
-  subnet_id      = aws_subnet.PrivateSubnet01AZ01.id
-  route_table_id = aws_route_table.PrivateRouteTable01.id
+resource "aws_route_table_association" "priv_sub01az01assoc" {
+  subnet_id      = aws_subnet.private_subnet01az01.id
+  route_table_id = aws_route_table.private_route_table01.id
 }
 
-resource "aws_route_table_association" "PrivSub01AZ02Assoc" {
-  subnet_id      = aws_subnet.PrivateSubnet01AZ02.id
-  route_table_id = aws_route_table.PrivateRouteTable01.id
+resource "aws_route_table_association" "priv_sub01az02assoc" {
+  subnet_id      = aws_subnet.private_subnet01az02.id
+  route_table_id = aws_route_table.private_route_table01.id
 }
-# Finish out security groups, test the networking deployment and then start working on the scripts for the actual infrastructure deployments.
 
 resource "aws_db_subnet_group" "db_subnets" {
   name       = "db_subnets"
-  subnet_ids = [aws_subnet.PrivateSubnet01AZ01.id, aws_subnet.PrivateSubnet01AZ02.id, aws_subnet.PublicSubnet01AZ02.id, aws_subnet.PublicSubnet01AZ01.id]
+  subnet_ids = [aws_subnet.private_subnet01az01.id, aws_subnet.private_subnet01az02.id, aws_subnet.public_subnet01az02.id, aws_subnet.public_subnet01az01.id]
 
   tags = {
     Name = "db_subnets"
@@ -118,18 +117,18 @@ resource "aws_db_subnet_group" "db_subnets" {
 }
 
 
-resource "aws_security_group" "ALBSG01" {
-  name        = "ALBSG01"
+resource "aws_security_group" "albsg01" {
+  name        = "albsg01"
   description = "ALB security group."
   vpc_id      = aws_vpc.main.id
 
   tags = {
-    Name = "ALBSG01"
+    Name = "albsg01"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http_traffic_ipv4_alb" {
-  security_group_id = aws_security_group.ALBSG01.id
+  security_group_id = aws_security_group.albsg01.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp" 
@@ -137,7 +136,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_traffic_ipv4_alb" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_https_traffic_ipv4_alb" {
-  security_group_id = aws_security_group.ALBSG01.id
+  security_group_id = aws_security_group.albsg01.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp" 
@@ -145,69 +144,69 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https_traffic_ipv4_alb" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_alb" {
-  security_group_id = aws_security_group.ALBSG01.id
+  security_group_id = aws_security_group.albsg01.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" 
 }
 
-resource "aws_security_group" "ASGSG01" {
-  name        = "ASGSG01"
+resource "aws_security_group" "asgsg01" {
+  name        = "asgsg01"
   description = "ASG security group."
   vpc_id      = aws_vpc.main.id
 
   tags = {
-    Name = "ASGSG01"
+    Name = "asgsg01"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http_from_alb" {
-  security_group_id        = aws_security_group.ASGSG01.id  
+  security_group_id        = aws_security_group.asgsg01.id  
   from_port                = 80
   to_port                  = 80
   ip_protocol                 = "tcp"
-  referenced_security_group_id = aws_security_group.ALBSG01.id
+  referenced_security_group_id = aws_security_group.albsg01.id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_https_from_alb" {
-  security_group_id        = aws_security_group.ASGSG01.id  
+  security_group_id        = aws_security_group.asgsg01.id  
   from_port                = 443
   to_port                  = 443
   ip_protocol                 = "tcp"
-  referenced_security_group_id = aws_security_group.ALBSG01.id
+  referenced_security_group_id = aws_security_group.albsg01.id
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_asg" {
-  security_group_id = aws_security_group.ASGSG01.id
+  security_group_id = aws_security_group.asgsg01.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" 
 }
 
-resource "aws_security_group" "DBSG01" {
-  name        = "DBSG01"
+resource "aws_security_group" "dbsg01" {
+  name        = "dbsg01"
   description = "DB security group."
   vpc_id      = aws_vpc.main.id
 
   tags = {
-    Name = "DBSG01"
+    Name = "dbsg01"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_PostreSQL_from_asg" {
-  security_group_id        = aws_security_group.DBSG01.id  
+  security_group_id        = aws_security_group.dbsg01.id  
   from_port                = 5432
   to_port                  = 5432
   ip_protocol                 = "tcp"
-  referenced_security_group_id = aws_security_group.ASGSG01.id
+  referenced_security_group_id = aws_security_group.asgsg01.id
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_db" {
-  security_group_id = aws_security_group.DBSG01.id
+  security_group_id = aws_security_group.dbsg01.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" 
 }
 
-resource "aws_lb_target_group" "TG01" {
-  name     = "TG01"
+resource "aws_lb_target_group" "tg01" {
+  name     = "tg01"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
@@ -224,32 +223,32 @@ resource "aws_lb_target_group" "TG01" {
   }
 }
 
-resource "aws_lb" "ALB01" {
-  name               = "ALB01"
+resource "aws_lb" "alb01" {
+  name               = "alb01"
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.ALBSG01.id]
-  subnets            = [aws_subnet.PublicSubnet01AZ02.id, aws_subnet.PublicSubnet01AZ01.id]
+  security_groups    = [aws_security_group.albsg01.id]
+  subnets            = [aws_subnet.public_subnet01az02.id, aws_subnet.public_subnet01az01.id]
 
   internal = false
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.ALB01.arn
+  load_balancer_arn = aws_lb.alb01.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.TG01.arn
+    target_group_arn = aws_lb_target_group.tg01.arn
   }
 }
 
-resource "aws_launch_template" "LT01" {
-  name_prefix   = "LT01"
+resource "aws_launch_template" "lt01" {
+  name_prefix   = "lt01"
   image_id      = "ami-0ebf411a80b6b22cb"
   instance_type = "t3.micro"
 
-  vpc_security_group_ids = [aws_security_group.ASGSG01.id]
+  vpc_security_group_ids = [aws_security_group.asgsg01.id]
 
   user_data = base64encode(templatefile("${path.module}/user_data.sh", {
   db_host = aws_db_instance.postgres.address
@@ -258,26 +257,26 @@ resource "aws_launch_template" "LT01" {
 }
 
 
-resource "aws_autoscaling_group" "ASG01" {
-  name                = "ASG01"
+resource "aws_autoscaling_group" "asg01" {
+  name                = "asg01"
   desired_capacity    = 2
   min_size            = 2
   max_size            = 3
-  vpc_zone_identifier = [aws_subnet.PrivateSubnet01AZ02.id, aws_subnet.PrivateSubnet01AZ01.id]
+  vpc_zone_identifier = [aws_subnet.private_subnet01az02.id, aws_subnet.private_subnet01az01.id]
 
   launch_template {
-    id      = aws_launch_template.LT01.id
+    id      = aws_launch_template.lt01.id
     version = "$Latest"
   }
 
-  target_group_arns = [aws_lb_target_group.TG01.arn]
+  target_group_arns = [aws_lb_target_group.tg01.arn]
 
   health_check_type         = "ELB"
   health_check_grace_period = 120
 
   tag {
     key                 = "Name"
-    value               = "ASG01"
+    value               = "asg01"
     propagate_at_launch = true
   }
 }
@@ -285,7 +284,7 @@ resource "aws_autoscaling_group" "ASG01" {
 
 
 resource "aws_db_instance" "postgres" {
-  identifier = "DB01"
+  identifier = "db01"
 
   engine         = "postgres"
   engine_version = "16.1"           
@@ -294,12 +293,12 @@ resource "aws_db_instance" "postgres" {
   allocated_storage = 20
   storage_type      = "gp3"
 
-  db_name  = "DB01"
+  db_name  = "db01"
   username = "username"
   password = "password"
 
   db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
-  vpc_security_group_ids = [aws_security_group.DBSG01.id]
+  vpc_security_group_ids = [aws_security_group.dbsg01.id]
 
   publicly_accessible = false
 
@@ -310,7 +309,7 @@ resource "aws_db_instance" "postgres" {
   multi_az                = false
 
   tags = {
-    Name = "DB01"
+    Name = "db01"
   }
 }
 
