6aab3cefd7ab902ae4c0cc560748fecf93df9f6a
Cole Detrick <153991478+coledetrick@users.noreply.github.com>
2026-01-11 20:44:28 -0600

    Update providers.tf

--- AWS/Terraform/dev/providers.tf
+++ AWS/Terraform/dev/providers.tf
@@ -11,13 +11,3 @@ provider "aws" {
   region = "us-west-2"
 }
 
-terraform {
-  backend "s3" {
-    bucket       = "tf-state-bucket-gha-bucket"
-    key          = "AWS/Terraform/dev/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
-
