45f05c4a85ea06e3fbe7eb80bfcd39afe04ea850
Cole Detrick <153991478+coledetrick@users.noreply.github.com>
2026-01-27 18:48:22 -0600

    Update backend.tf

--- AWS/Terraform/dev/backend.tf
+++ AWS/Terraform/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "tf-state-bucket-gha-bucket-lab"
     key          = "AWS/Terraform/dev/terraform.tfstate"
-    region       = "us-east-1"
+    region       = "us-east-2"
     encrypt      = true
     use_lockfile = true
   }
